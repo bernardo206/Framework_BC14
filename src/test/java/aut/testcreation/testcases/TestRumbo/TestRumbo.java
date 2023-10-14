289804503add416710d38d9fc011cae1eb7cf57b
bernado206 <bernardo_88_3_@hotmail.com>
2023-10-14 16:05:01 -0300

    orden de los test

--- src/test/java/aut/testcreation/testcases/TestRumbo/TestRumbo.java
+++ src/test/java/aut/testcreation/testcases/TestRumbo/TestRumbo.java
@@ -72,217 +72,6 @@ public class TestRumbo {
     }
 
     @Test
-    public void CP0012_Reserva_vuelo_solo_ida_OK() throws InterruptedException {
-        home.aceptarCookies();
-        home.ingresarOrigen();
-        home.ingresarDestino();
-        home.seleccionarVueloSoloIda();
-        home.ingresarFechaSalida();
-        home.buscar();
-        vuelosPage.seleccionarVuelo();
-        vuelosPage.confirmarVuelo();
-        vuelosPage.elegirTarifa("Elegir Classic");
-        contacto.completarDatosContacto("Juan", "Perez", "elasdelaspruebasautomatizadas@yahoo.com.es", "2224545454", "mi calle", 1234, "70000", "Barcelona", "10", "enero", "2000");
-        contacto.selecionaEquipaje();
-        contacto.confirma();
-        seguro.seleccionarSeguro();
-        seguro.confirmarSeguro();
-        Boolean resultadoEsperado = true;
-        Boolean resultadoObtenido = confirmacion.confirmacion();
-        Assertions.assertEquals(resultadoEsperado, resultadoObtenido);
-
-    }
-
-    @Test
-    public void CP0008_Reserva_vuelo_Sin_Seleccionar_Equipaje_NO_OK() throws InterruptedException {
-        home.aceptarCookies();
-        home.ingresarOrigen();
-        home.ingresarDestino();
-        home.seleccionarVueloSoloIda();
-        home.ingresarFechaSalida();
-        home.buscar();
-        vuelosPage.seleccionarVuelo();
-        vuelosPage.confirmarVuelo();
-        vuelosPage.elegirTarifa("Elegir Classic");
-        contacto.completarDatosContacto("Juan", "Perez", "elasdelaspruebasautomatizadas@yahoo.com.es", "2224545454", "mi calle", 1234, "70000", "Barcelona", "10", "enero", "2000");
-        contacto.confirma();
-        contacto.detectaFaltaSelecionEquipaje();
-        Boolean resultadoEsperado = true;
-        Boolean resultadoObtenido = contacto.detectaFaltaSelecionEquipaje();
-        Assertions.assertEquals(resultadoEsperado, resultadoObtenido);
-    }
-
-   @Test
-    public void CP009_Reserva_Otro_Idioma_OK() throws InterruptedException {
-        home.aceptarCookies();
-        home.seleccionarPais();
-        home.aceptarCookiesIngles();
-        home.selectOneWay();
-        Boolean resultadoEsperado = true;
-        Boolean resultadoObtenido = contacto.detectaFaltaSelecionEquipaje();
-        Assertions.assertEquals(resultadoEsperado, resultadoObtenido);
-
-    }
-
-    @Test
-    public void CP0018_Busqueda_hotel_OK() {
-        home.aceptarCookies();
-        hotelPage.maximizarBrowser();
-        hotelPage.completarCamposBusquedaHoteles("Barcelona");
-
-    }
-
-    @Test
-    public void CP0017_Busqueda_Hotel_Fecha_no_Ok()  {
-
-        home.aceptarCookies();
-        hotelPage.maximizarBrowser();
-        hotelPage.completarCamposBusquedaHoteles_fecha_deshabilitada("Barcelona");
-
-
-    }
-
-    @Test
-    public void CP0015_Error_Ofertas_Trenes_Renfe(){
-
-        home.maximizarBrowser();
-        home.esperarXSegundos(2000);
-        home.aceptarCookies();
-        home.locatorVerMas();
-        home.esperarXSegundos(2000);
-        home.btnTrenes();
-        home.esperarXSegundos(2000);
-        trenPage.entrarOfertasTrenesRenfe();
-        trenPage.esperarXSegundos(6000);
-        trenPage.identificarPestaniasOfertasRenfeTrenes();
-        trenPage.esperarXSegundos(2000);
-        trenPage.esperarXSegundos(8000);
-
-
-    }
-
-
-    @Test
-    public void CP004_Reserva_TrenHotel_Fechas_no_disponibles_NO_OK() throws InterruptedException {
-        home.aceptarCookies();
-        home.locatorVerMas();
-        home.btnTrenHotel();
-        hotelTren.seleccionarOrigen();
-        hotelTren.seleccionarDestino();
-        hotelTren.ingresarFechaSalidaLarga();
-        hotelTren.detectaErrorFecha();
-        Assertions.assertTrue(true);
-    }
-
-    @Test
-    public void CP007_Vuelo_Fechas_no_disponibles_NO_OK() throws InterruptedException {
-        home.aceptarCookies();
-        home.ingresarOrigen();
-        home.ingresarDestino();
-        home.seleccionarVueloSoloIda();
-        home.ingresarFechaSalidaLarga();
-        home.detectaErrorFecha();
-        Assertions.assertTrue(true);
-
-    }
-
-
-
-    @Test
-    public void CP0011_Busqueda_Vuelo_Filtro_Mas_Barato_OK() throws InterruptedException {
-        home.aceptarCookies();
-        home.ingresarOrigen();
-        home.ingresarDestino();
-        home.seleccionarVueloSoloIda();
-        home.ingresarFechaSalida();
-        home.buscar();
-        vuelosPage.filtroMasBarato();
-        Assertions.assertTrue(true);
-    }
-
-    @Test
-    public void CP006_Busqueda_Tren_Filtro_Mas_Rapido_OK() throws InterruptedException {
-        home.maximizarBrowser();
-        home.aceptarCookies();
-        home.locatorVerMas();
-        home.btnTrenes();
-        trenPage.locatorIdaVuelta();
-        trenPage.locatorOrigen("Madrid");
-        trenPage.locatorDestino();
-        trenPage.btnBuscar();
-        trenPage.filtroMasRapido();
-        Assertions.assertTrue(true);
-
-    }
-
-    @Test
-    public void CP0016_BusquedaHotelSinIndicarDestino_NO_OK () throws InterruptedException {
-        home.aceptarCookies();
-        hotelPage.maximizarBrowser();
-        hotelPage.completarCamposBusquedaHoteles(" ");
-        Assertions.assertTrue(true);
-    }
-
-
-
-    @Test
-    public void CP010_TarjetaDeRegalo_Compra_OK(){
-        home.aceptarCookies();
-        home.locatorVerMas();
-        tarjeta.BtnTarjetaRegalo();
-        regala.Regala();
-        particular.Importe();
-        particular.Imprimir();
-        particular.Destinatario();
-        particular.correo();
-        particular.Remitente();
-        particular.Añadir();
-        particular.Continuar();
-
-        Assertions.assertTrue(true);
-
-    }
-    @Test
-    public void CP0014_Reserva_paquete_HotelMasvuelo_OK() throws InterruptedException {
-        home.aceptarCookies();
-        vueloMasHotel.VueloMasHotel();
-        vueloMasHotel.OrigenVuelo();
-        vueloMasHotel.DestinoVuelo();
-        vueloMasHotel.FechaDeIda();
-        vueloMasHotel.localizar();
-        alojamiento.ElegirAlojamiento();//Hotel Flavia
-        room.Continuar1ero(1);
-
-
-    }
-
-
-
-
-    @Test
-    public void CP005_ReservaTrenIda(){  //no elijo q pasajero, lo hace con 2 que es la opcion recomendada
-        home.maximizarBrowser();
-        home.aceptarCookies();
-        home.locatorVerMas();
-        home.btnTrenes();
-        tren.locatorIda();
-        tren.locatorOrigen("Madrid");
-        tren.locatorDestino();
-        tren.btnBuscar();
-        tren.locatorPrimerTren();
-        tren.aceptarPrimerTren();
-
-        formularioT.formularioDatos("carlos", "perez", "probando@gmail.com", 234343434);
-        // formularioT.formulacioPasajero("Roberto","Garcia",14,2000,"123456789b");
-
-        formularioT.formularioDatos("carlos","perez","probando@gmail.com", 234343434);
-        formularioT.formulacioPasajero("Roberto","Garcia",14,1988,"123456789b");
-
-
-        Assertions.assertTrue(true);
-    }
-
-    @Test
     public void CP001_ReservaTren_DestinoNoDisponible() {
         home.maximizarBrowser();
         home.aceptarCookies();
@@ -325,6 +114,235 @@ public class TestRumbo {
         tren.btnBuscar();
         Assertions.assertTrue(true);
     }
+
+
+    @Test
+    public void CP004_Reserva_TrenHotel_Fechas_no_disponibles_NO_OK() throws InterruptedException {
+        home.aceptarCookies();
+        home.locatorVerMas();
+        home.btnTrenHotel();
+        hotelTren.seleccionarOrigen();
+        hotelTren.seleccionarDestino();
+        hotelTren.ingresarFechaSalidaLarga();
+        hotelTren.detectaErrorFecha();
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    public void CP005_ReservaTrenIda(){  //no elijo q pasajero, lo hace con 2 que es la opcion recomendada
+        home.maximizarBrowser();
+        home.aceptarCookies();
+        home.locatorVerMas();
+        home.btnTrenes();
+        tren.locatorIda();
+        tren.locatorOrigen("Madrid");
+        tren.locatorDestino();
+        tren.btnBuscar();
+        tren.locatorPrimerTren();
+        tren.aceptarPrimerTren();
+
+        formularioT.formularioDatos("carlos", "perez", "probando@gmail.com", 234343434);
+        // formularioT.formulacioPasajero("Roberto","Garcia",14,2000,"123456789b");
+
+        formularioT.formularioDatos("carlos","perez","probando@gmail.com", 234343434);
+        formularioT.formulacioPasajero("Roberto","Garcia",14,1988,"123456789b");
+
+
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    public void CP006_Busqueda_Tren_Filtro_Mas_Rapido_OK() throws InterruptedException {
+        home.maximizarBrowser();
+        home.aceptarCookies();
+        home.locatorVerMas();
+        home.btnTrenes();
+        trenPage.locatorIdaVuelta();
+        trenPage.locatorOrigen("Madrid");
+        trenPage.locatorDestino();
+        trenPage.btnBuscar();
+        trenPage.filtroMasRapido();
+        Assertions.assertTrue(true);
+
+    }
+
+    @Test
+    public void CP007_Vuelo_Fechas_no_disponibles_NO_OK() throws InterruptedException {
+        home.aceptarCookies();
+        home.ingresarOrigen();
+        home.ingresarDestino();
+        home.seleccionarVueloSoloIda();
+        home.ingresarFechaSalidaLarga();
+        home.detectaErrorFecha();
+        Assertions.assertTrue(true);
+
+    }
+
+    @Test
+    public void CP008_Reserva_vuelo_Sin_Seleccionar_Equipaje_NO_OK() throws InterruptedException {
+        home.aceptarCookies();
+        home.ingresarOrigen();
+        home.ingresarDestino();
+        home.seleccionarVueloSoloIda();
+        home.ingresarFechaSalida();
+        home.buscar();
+        vuelosPage.seleccionarVuelo();
+        vuelosPage.confirmarVuelo();
+        vuelosPage.elegirTarifa("Elegir Classic");
+        contacto.completarDatosContacto("Juan", "Perez", "elasdelaspruebasautomatizadas@yahoo.com.es", "2224545454", "mi calle", 1234, "70000", "Barcelona", "10", "enero", "2000");
+        contacto.confirma();
+        contacto.detectaFaltaSelecionEquipaje();
+        Boolean resultadoEsperado = true;
+        Boolean resultadoObtenido = contacto.detectaFaltaSelecionEquipaje();
+        Assertions.assertEquals(resultadoEsperado, resultadoObtenido);
+    }
+
+    @Test
+    public void CP009_Reserva_Otro_Idioma_OK() throws InterruptedException {
+        home.aceptarCookies();
+        home.seleccionarPais();
+        home.aceptarCookiesIngles();
+        home.selectOneWay();
+        Boolean resultadoEsperado = true;
+        Boolean resultadoObtenido = contacto.detectaFaltaSelecionEquipaje();
+        Assertions.assertEquals(resultadoEsperado, resultadoObtenido);
+
+    }
+
+    @Test
+    public void CP0010_TarjetaDeRegalo_Compra_OK(){
+        home.aceptarCookies();
+        home.locatorVerMas();
+        tarjeta.BtnTarjetaRegalo();
+        regala.Regala();
+        particular.Importe();
+        particular.Imprimir();
+        particular.Destinatario();
+        particular.correo();
+        particular.Remitente();
+        particular.Añadir();
+        particular.Continuar();
+
+        Assertions.assertTrue(true);
+
+    }
+
+    @Test
+    public void CP0011_Busqueda_Vuelo_Filtro_Mas_Barato_OK() throws InterruptedException {
+        home.aceptarCookies();
+        home.ingresarOrigen();
+        home.ingresarDestino();
+        home.seleccionarVueloSoloIda();
+        home.ingresarFechaSalida();
+        home.buscar();
+        vuelosPage.filtroMasBarato();
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    public void CP0012_Reserva_vuelo_solo_ida_OK() throws InterruptedException {
+        home.aceptarCookies();
+        home.ingresarOrigen();
+        home.ingresarDestino();
+        home.seleccionarVueloSoloIda();
+        home.ingresarFechaSalida();
+        home.buscar();
+        vuelosPage.seleccionarVuelo();
+        vuelosPage.confirmarVuelo();
+        vuelosPage.elegirTarifa("Elegir Classic");
+        contacto.completarDatosContacto("Juan", "Perez", "elasdelaspruebasautomatizadas@yahoo.com.es", "2224545454", "mi calle", 1234, "70000", "Barcelona", "10", "enero", "2000");
+        contacto.selecionaEquipaje();
+        contacto.confirma();
+        seguro.seleccionarSeguro();
+        seguro.confirmarSeguro();
+        Boolean resultadoEsperado = true;
+        Boolean resultadoObtenido = confirmacion.confirmacion();
+        Assertions.assertEquals(resultadoEsperado, resultadoObtenido);
+
+    }
+
+    @Test
+    public void CP0013_BusquedaHotelSinIndicarDestino_NO_OK () throws InterruptedException {
+        home.aceptarCookies();
+        hotelPage.maximizarBrowser();
+        hotelPage.completarCamposBusquedaHoteles(" ");
+        Assertions.assertTrue(true);
+    }
+
+
+    @Test
+    public void CP0014_Reserva_paquete_HotelMasvuelo_OK() throws InterruptedException {
+        home.aceptarCookies();
+        vueloMasHotel.VueloMasHotel();
+        vueloMasHotel.OrigenVuelo();
+        vueloMasHotel.DestinoVuelo();
+        vueloMasHotel.FechaDeIda();
+        vueloMasHotel.localizar();
+        alojamiento.ElegirAlojamiento();//Hotel Flavia
+        room.Continuar1ero(1);
+
+    }
+
+    @Test
+    public void CP0015_Error_Ofertas_Trenes_Renfe(){
+
+        home.maximizarBrowser();
+        home.esperarXSegundos(2000);
+        home.aceptarCookies();
+        home.locatorVerMas();
+        home.esperarXSegundos(2000);
+        home.btnTrenes();
+        home.esperarXSegundos(2000);
+        trenPage.entrarOfertasTrenesRenfe();
+        trenPage.esperarXSegundos(6000);
+        trenPage.identificarPestaniasOfertasRenfeTrenes();
+        trenPage.esperarXSegundos(2000);
+        trenPage.esperarXSegundos(8000);
+
+    }
+
+    @Test
+    public void CP0017_Busqueda_Hotel_Fecha_no_Ok()  {
+
+        home.aceptarCookies();
+        hotelPage.maximizarBrowser();
+        hotelPage.completarCamposBusquedaHoteles_fecha_deshabilitada("Barcelona");
+
+    }
+
+    @Test
+    public void CP0018_Busqueda_hotel_OK() {
+        home.aceptarCookies();
+        hotelPage.maximizarBrowser();
+        hotelPage.completarCamposBusquedaHoteles("Barcelona");
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
